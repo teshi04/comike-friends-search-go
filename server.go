7e78d66821c1fb896c6b0662bb81354ad8533b31
teshi04 <teshi04@gmail.com>
2016-12-10 14:53:26 +0900

    フォローしている人を取得

--- server.go
+++ server.go
@@ -1,11 +1,46 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"net/url"
 
+	"github.com/ChimeraCoder/anaconda"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
+
+func GetAllFriends(screenName string) {
+	anaconda.SetConsumerKey("")
+	anaconda.SetConsumerSecret("")
+
+	api := anaconda.NewTwitterApi("", "")
+
+	friendIds := []anaconda.User{}
+	v := url.Values{}
+	v.Set("count", "100")
+	v.Set("screen_name", screenName)
+	nextCursor := "-1"
+	for nextCursor != "0" {
+		v.Set("cursor", nextCursor)
+		c, err := api.GetFriendsList(v)
+		if err != nil {
+			log.Fatal(err)
+		}
+		friendIds = append(friendIds, c.Users...)
+		fmt.Print(c.Users[0].ScreenName)
+		nextCursor = c.Next_cursor_str
+	}
+
+}
+
+func search(c echo.Context) error {
+	screenName := c.FormValue("query")
+	GetAllFriends(screenName)
+
+	return c.HTML(http.StatusOK, fmt.Sprintf("%s", screenName))
+}
 
 func main() {
 	e := echo.New()
@@ -16,6 +51,8 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!\n")
 	})
+	e.Static("/", "public")
+	e.POST("/", search)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
